0101c5d77a57230937a02ad9c25e89c5a3d059af
tanmaygoel <timgoelny@gmail.com>
2025-08-14 19:12:12 -0400

    added api for source specific sentiment info

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from scheduler import start_scheduler
 from db import get_db
 from sqlalchemy.orm import Session
 from models import NewsItem
+from rss_fetcher import RSS_Feeds
 
 app = FastAPI(title="Macrofactor News App")
 
@@ -22,9 +23,38 @@ def get_sentiment_data(db: Session = Depends(get_db)):
         .group_by(NewsItem.sentiment)
         .all()
     )
+    #returns list of tuples
 
     result = {}
     for sentiment, count in sentiment_counts:
         result[sentiment] = count
+
+    return result
+
+@app.get("/news/sources")
+def get_source_data(db: Session = Depends(get_db)):
+    result = []
+    #list of dictionaries
+    for feed in RSS_Feeds:
+        query = (
+            db.query(NewsItem.sentiment, func.count(NewsItem.id))
+            .filter(NewsItem.source == feed)
+            .group_by(NewsItem.sentiment)
+            .all()
+        )
+
+        source = {    
+            "negative": 0,
+            "positive": 0,
+            "neutral": 0
+            }
+        #default values
+        
+        for sent, count in query:
+            source[sent] = count
+
+        source["source"] = feed
+        
+        result.append(source)
 
     return result
