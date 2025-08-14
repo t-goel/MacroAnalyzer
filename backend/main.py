94c960b8f32e29427941978defd66b4833e7d76d
tanmaygoel <timgoelny@gmail.com>
2025-08-14 18:16:57 -0400

    implemented api for sentiment data

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import func
 from scheduler import start_scheduler
-from models import Base
-from db import engine
+from db import get_db
+from sqlalchemy.orm import Session
+from models import NewsItem
 
-app = FastAPI()
+app = FastAPI(title="Macrofactor News App")
 
 
 start_scheduler()
@@ -11,3 +13,18 @@ start_scheduler()
 @app.get("/")
 def root():
     return {"message": "MacroFactor API is live and fetching news"}
+
+
+@app.get("/news/sentiment")
+def get_sentiment_data(db: Session = Depends(get_db)):
+    sentiment_counts = (
+        db.query(NewsItem.sentiment, func.count(NewsItem.id))
+        .group_by(NewsItem.sentiment)
+        .all()
+    )
+
+    result = {}
+    for sentiment, count in sentiment_counts:
+        result[sentiment] = count
+
+    return result
